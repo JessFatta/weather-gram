984b873419f93b79046a1d0798b9efcfa81145ad
Jessica Fatta <jessicafatta@gmail.com>
2022-04-11 13:04:14 -0400

    Add nav to test rendering

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+//import { Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Nav from '../Nav/Nav'
+import SearchBar from '../SearchBar/SearchBar';
 
 
 class App extends Component {
-  constructor() {
-    super()
+  constructor(props: {}) {
+    super(props)
     this.state = {
-
+      ideas: []
     }
   }
 
   render() {
     return (
       <div>
-        
+        <Nav />
       </div>
     )
   }
