cbf02e0304ac0375e2d78bd70b6315689738ae1b
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-11 16:12:50 -0600

    Add fetch calls for 3Day and 7Day forecasts

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,18 @@
-const getWeatherData = (location) => {
+const getCurrentData = (location) => {
   return fetch(`http://api.weatherapi.com/v1/current.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}`)
   .then(response => response.json())
 }
 
-export { getWeatherData }
+const get3DayData = (location) => {
+  return fetch(`http://api.weatherapi.com/v1/forecast.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}&days=3&aqi=no&alerts=no`)
+  .then(response => response.json())
+}
+
+const get7DayData = (location) => {
+  return fetch(`http://api.weatherapi.com/v1/forecast.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}&days=7&aqi=no&alerts=no`)
+  .then(response => response.json())
+}
+
+
+
+export { getCurrentData, get3DayData, get7DayData }
