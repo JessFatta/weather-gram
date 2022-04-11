bbe1d8a228996b44777c4b2acd15fa09f51123fe
Jessica Fatta <jessicafatta@gmail.com>
2022-04-11 12:01:37 -0400

    Rearrange App file structure

---
+++ src/App/App.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
