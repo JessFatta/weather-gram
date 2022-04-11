bf381c96d428f945e132dea5b7975f47642e4b5e
Jessica Fatta <jessicafatta@gmail.com>
2022-04-11 14:04:53 -0400

    Write function to connect api calls

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,20 +4,30 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from '../Nav/Nav'
 import SearchBar from '../SearchBar/SearchBar';
+import { getWeatherData } from '../apiCalls'
+
+type AppState = {
+  weather: {}
+}
 
 
-class App extends Component {
-  constructor(props: {}) {
-    super(props)
-    this.state = {
-      ideas: []
+class App extends Component <{}, AppState> {
+    state: AppState = {
+      weather: {}
     }
+
+
+  setLocation = (location: string) => {
+    console.log('heyyyy')
+    getWeatherData(location)
+    .then(data => this.setState({weather: data}))
+    .then(() => console.log(this.state.weather))
   }
 
   render() {
     return (
       <div>
-        <Nav />
+        <Nav setLocation={this.setLocation}/>
       </div>
     )
   }
