f1b631a917b7ea68508d3848736090fd9b4bfa2b
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-11 19:47:40 -0600

    Refactor types and add props

--- src/ForecastCard/ForecastCard.tsx
+++ src/ForecastCard/ForecastCard.tsx
@@ -3,31 +3,25 @@ import './ForecastCard.css'
 
 type ForecastCardProps = {
   location: string,
-  // forecast: {
-    // forecastday: [
-    //   {
-        // date: string,
-        // day: {
-          maxtemp_f: null,
-          mintemp_f: null,
-          // avghumidity: null,
-          // daily_chance_of_rain: null,
-          // condition: {
-          //   text: string,
-          //   icon: string
-          // }
-        // }
-      }
-    // ]
-  // }
-// }
+  date: string,
+  maxtemp_f: number,
+  mintemp_f: number,
+  avghumidity: number,
+  daily_chance_of_rain: number,
+  text: string,
+  icon: string
+}
 
-const ForecastCard: React.FC <ForecastCardProps> =({location, maxtemp_f, mintemp_f}) => {
+const ForecastCard: React.FC <ForecastCardProps> =({location, maxtemp_f, mintemp_f, avghumidity, daily_chance_of_rain, text, icon, date}) => {
   return (
     <div className="forecast-card">
+      <img src={icon} alt={text} />
       <p>Location: {location}</p>
-      <p>Today's High: {maxtemp_f} F</p>
-      <p>Today's Low: {mintemp_f} F</p>
+      <p>Date: {date}</p>
+      <p>High: {maxtemp_f} F</p>
+      <p>Low: {mintemp_f} F</p>
+      <p>Average Humidity: {avghumidity}%</p>
+      <p>Chance of Rain: {daily_chance_of_rain}%</p>
     </div>
   )
 }
