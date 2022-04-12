8d34144a35de90c629229fc232c25c95cf00a71c
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-12 09:45:25 -0600

    Remove SevenDay API call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,11 +8,5 @@ const get3DayData = (location) => {
   .then(response => response.json())
 }
 
-const get7DayData = (location) => {
-  return fetch(`http://api.weatherapi.com/v1/forecast.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}&days=7&aqi=no&alerts=no`)
-  .then(response => response.json())
-}
 
-
-
-export { getCurrentData, get3DayData, get7DayData }
+export { getCurrentData, get3DayData }
