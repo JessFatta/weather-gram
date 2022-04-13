ce3d5e96ff3b20cb97fae5f7adc4b896e4c7b733
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-13 09:39:30 -0600

    Add navLink to random button

--- cypress/integration/nav_test.js
+++ cypress/integration/nav_test.js
@@ -10,6 +10,30 @@ describe('Nav Bar', () => {
   })
 
   it('As a user, I should be able to click the Let\'s go! button', () => {
-    cy.get('div[class="search-bar-container"]').get('button').first().click()
+    cy.get('button[class="lets-go-button"]').click()
+  })
+
+  it('As a user, I should be able to click the 3 day button and see a 3 day forecast for that location', () => {
+    cy.get('button[class="3day-button"]').click()
+
+    .url().should('eq', 'http://localhost:3000/3Day')
+  })
+
+  it('As a user, I should be able to click the home button and return to the main view with the current weather for that location', () => {
+    cy.get('button[class="home-button"]').click()
+
+    .url().should('eq', 'http://localhost:3000/')
+  })
+
+  it('As a user, I should be able to click the favorites button and route to the favorites page', () => {
+    cy.get('button[class="favorites-button"]').click()
+
+    .url().should('eq', 'http://localhost:3000/favorites')
+  })
+
+  it('As a user, I should be able to click the random button and return to the main view with a new random city displayed', () => {
+    cy.get('button[class="random-button"]').click()
+
+    .url().should('eq', 'http://localhost:3000/')
   })
 })
