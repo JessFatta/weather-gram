df5672d8701f50e62f44c6649bb56b21c8e5a6e9
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-13 19:23:00 -0400

    Add Footer and style

---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Footer.css"
+
+
+const Footer = () => {
+  return(
+    <footer>
+      <p className="footer-text">©2022 Weather Gram</p>
+      <p className="footer-text">|</p>
+      <p className="footer-text">A very real Weather site by Andrew, Daniel, Jess, Emily</p>
+    </footer>
+
+  )
+}
+
+
+export default Footer
