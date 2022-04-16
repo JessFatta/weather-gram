f8e50d15f6444a9c4dcd7464fce480e3dd5448e4
Jessica Fatta <jessicafatta@gmail.com>
2022-04-16 11:47:06 -0400

    Add sad path for server error

--- cypress/integration/main_test.js
+++ cypress/integration/main_test.js
@@ -7,9 +7,9 @@ describe('Main', () => {
     cy.visit('http://localhost:3000/')
         .get('div[class="card"]')
         .should('contain', 'Location: London')
-        .should('contain', 'Temperature: 50 F')
+        .should('contain', 'Temperature: 50° F')
         .should('contain', 'Conditions: Rainy')
-        .should('contain', 'Feels Like: 51 F')
+        .should('contain', 'Feels Like: 51° F')
         .should('contain', 'Humidity: 30%')
         .should('contain', 'Wind: 15 mph')
         .should('contain', 'UV Index: 3')
@@ -41,4 +41,15 @@ describe('Main', () => {
         .should('contain', '©2022 Weather Gram')
         .should('contain', 'A very real Weather site by Andrew, Daniel, Jess, Emily')
     })
+
+    it('As a user, if the server is down, I will see an error', () => {
+      cy.intercept('GET', 'http://api.weatherapi.com/v1/*', {
+          statusCode: 400,
+          ok: false
+        }).as('matchedUrl')
+        cy.visit('http://localhost:3000/')
+        .get('div[class="loading-error-message"]')
+        .contains('Something went wrong! Send complaints to Scott and Robbie')
+  })
+  
 })
