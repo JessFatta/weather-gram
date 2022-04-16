7d29cef77f26deed1fb50f6408e4b9c5331ae3c9
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-15 18:19:42 -0600

    Add degree symbol

--- src/ForecastCard/ForecastCard.tsx
+++ src/ForecastCard/ForecastCard.tsx
@@ -9,8 +9,8 @@ const ForecastCard: React.FC <ForecastCardProps> =({location, maxtemp_f, mintemp
       <img src={icon} alt={text} />
       <p>Location: {location}</p>
       <p>Date: {date}</p>
-      <p>High: {maxtemp_f} F</p>
-      <p>Low: {mintemp_f} F</p>
+      <p>High: {maxtemp_f}° F</p>
+      <p>Low: {mintemp_f}° F</p>
       <p>Average Humidity: {avghumidity}%</p>
       <p>Chance of Rain: {daily_chance_of_rain}%</p>
     </div>
