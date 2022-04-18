94a027ba7e2d5cd277474802d18e971c8814ca2d
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-18 16:22:42 -0600

    Add dayjs to format date

--- src/ForecastCard/ForecastCard.tsx
+++ src/ForecastCard/ForecastCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ForecastCardProps } from '../Interfaces'
+import  dayjs from 'dayjs';
 import '../Card/Card.css'
 
 
@@ -8,7 +9,7 @@ const ForecastCard: React.FC <ForecastCardProps> =({location, maxtemp_f, mintemp
     <div className="card">
       <img src={icon} alt={text} />
       <p>Location: {location}</p>
-      <p>Date: {date}</p>
+      <p>Date: {dayjs(date).format('MMM DD, YYYY')}</p>
       <p>High: {maxtemp_f}° F</p>
       <p>Low: {mintemp_f}° F</p>
       <p>Average Humidity: {avghumidity}%</p>
