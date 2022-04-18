ede3b1d59479df93f296caab1ea558ff35aa7261
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-18 16:27:47 -0600

    Change button text

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -16,9 +16,9 @@ const Nav: React.FC <NavProps> = ({setLocation, getRandomCity}) => {
     </div>
       <section className='buttons-container'>
       <NavLink to="/"><button className='home-button'>Home</button></NavLink>
-    <NavLink to="/3Day"><button className='three-day-button'>3 Day</button></NavLink>
+    <NavLink to="/3Day"><button className='three-day-button'>3 Day Forecast</button></NavLink>
     <NavLink to='/favorites'><button className='favorites-button'>Saved Locations</button></NavLink>
-    <NavLink to="/"><button className='random-button' onClick={() => getRandomCity()}>Random</button></NavLink>
+    <NavLink to="/"><button className='random-button' onClick={() => getRandomCity()}>Explore</button></NavLink>
       </section>
     </nav>
   )
