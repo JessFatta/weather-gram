dccbcf0068caca40a9d0eeca913cd7877fe60132
danielsolomon332 <daniel.solomon332@gmail.com>
2022-04-18 19:18:30 -0600

    Refactor api urls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,10 @@
 const getCurrentData = (location) => {
-  return fetch(`http://api.weatherapi.com/v1/current.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}`)
+  return fetch(`https://api.weatherapi.com/v1/current.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}`)
   .then(response => handleResponse(response))
 }
 
 const get3DayData = (location) => {
-  return fetch(`http://api.weatherapi.com/v1/forecast.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}&days=3&aqi=no&alerts=no`)
+  return fetch(`https://api.weatherapi.com/v1/forecast.json?key=f7ac8cfa51bb4034b12220944220704&q=${location}&days=3&aqi=no&alerts=no`)
   .then(response => handleResponse(response))
 }
 
